6f11c9c33f029dfd0fd6b799ea038c72c900a899
Andreas Sahle <as@wunderkraut.com>
2013-06-07 10:20:29 +0200

    initial yeoman generator files and project setup

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-generator-drupaltheme
-=====================
+# Generator-drupaltheme
+[![Build Status](https://secure.travis-ci.org/pixelmord/generator-drupaltheme.png?branch=master)](https://travis-ci.org/pixelmord/generator-drupaltheme)
 
-Yeoman generator for a Drupal theme
+A generator for Yeoman.
+
+## Getting started
+- Make sure you have [yo](https://github.com/yeoman/yo) installed:
+    `npm install -g yo`
+- Install the generator: `npm install -g generator-drupaltheme`
+- Run: `yo drupaltheme`
+
+## License
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ app/index.js
@@ -0,0 +1,65 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var DrupalthemeGenerator = module.exports = function DrupalthemeGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(DrupalthemeGenerator, yeoman.generators.Base);
+
+DrupalthemeGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // welcome message
+  var welcome =
+  '\n     _-----_' +
+  '\n    |       |' +
+  '\n    |' + '--(o)--'.red + '|   .--------------------------.' +
+  '\n   `---------´  |    ' + 'Welcome to Yeoman,'.yellow.bold + '    |' +
+  '\n    ' + '( '.yellow + '_' + '´U`'.yellow + '_' + ' )'.yellow + '   |   ' + 'ladies and gentlemen!'.yellow.bold + '  |' +
+  '\n    /___A___\\   \'__________________________\'' +
+  '\n     |  ~  |'.yellow +
+  '\n   __' + '\'.___.\''.yellow + '__' +
+  '\n ´   ' + '`  |'.red + '° ' + '´ Y'.red + ' `\n';
+
+  console.log(welcome);
+
+  var prompts = [{
+    name: 'someOption',
+    message: 'Would you like to enable this option?',
+    default: 'Y/n',
+    warning: 'Yes: Enabling this will be totally awesome!'
+  }];
+
+  this.prompt(prompts, function (err, props) {
+    if (err) {
+      return this.emit('error', err);
+    }
+
+    this.someOption = (/y/i).test(props.someOption);
+
+    cb();
+  }.bind(this));
+};
+
+DrupalthemeGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/templates');
+
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+};
+
+DrupalthemeGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
