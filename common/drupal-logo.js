f1632ad3e6eff8fd00cb2c5e604a99aadc7d4dfc
Andreas Sahle <andreas.sahle@wunderkraut.com>
2013-12-04 00:59:51 +0100

    add drupal logo generator

---
+++ common/drupal-logo.js
@@ -0,0 +1,35 @@
+var chalk = require('chalk');
+
+module.exports.logo =
+chalk.blue('\n                            O                     ') +
+chalk.blue('\n                           8=$                    ') +
+chalk.blue('\n                          D.~$$8                  ') +
+chalk.blue('\n                        ,') + chalk.white('..') + chalk.blue('=$$$$$O                ') +
+chalk.blue('\n                      O') + chalk.white('...') + chalk.blue('~~$$$$$$$$$O            ') +
+chalk.blue('\n                   O') + chalk.white('.....') + chalk.blue('=~$$$$$$$$$$$$$          ') +
+chalk.blue('\n                 8') + chalk.white('......') + chalk.blue('==$$$$$$$$$$$$$$$$        ') +
+chalk.blue('\n               O') + chalk.white('......') + chalk.blue('=~$$$$$$$$$$$$$$$$$$$O      ') +
+chalk.blue('\n              ,') + chalk.white('....') + chalk.blue('===$$$$$$$$$$$$$$$$$$$$$$Z     ') +
+chalk.blue('\n             =') + chalk.white('..') + chalk.blue('~==$$$$$$$$$$$$$$$$$$$$$$$$$$O    ') +
+chalk.blue('\n            $~~=$$$$$$$$$$$$$$$$$$$$$$$$$$$$$OO   ') +
+chalk.blue('\n           O$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$OOO8  ') +
+chalk.blue('\n          $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ZOOOOOOOO ') +
+chalk.blue('\n          $$$$$$$$$$$$$') + chalk.white('......') + chalk.blue('$$$$$$$$$$OOOOOOOOOO ') +
+chalk.blue('\n          $$$$$$$$$$$') + chalk.white('..........') + chalk.blue(':$$$$$OOOOO') + chalk.white('.....') + chalk.blue('OO ') +
+chalk.blue('\n          8$$$$$$$$') + chalk.white('...............') + chalk.blue('$OOOO') + chalk.white('.........') + chalk.blue('O ') +
+chalk.blue('\n           $$$$$$$$') + chalk.white('............................') + chalk.blue('.  ') +
+chalk.blue('\n           O$$$$$$Z') + chalk.white('................') + chalk.blue('ZOZ') + chalk.white('.........') + chalk.blue('Z  ') +
+chalk.blue('\n            $$$$$$$') + chalk.white('.............') + chalk.blue('$OOOOOOZ') + chalk.white('......') + chalk.blue('.   ') +
+chalk.blue('\n             O$$$$$Z') + chalk.white('.........') + chalk.blue('OOOO') + chalk.white('.....') + chalk.blue('OOO') + chalk.white('... ') + chalk.blue('.    ') +
+chalk.blue('\n              OOOOOOOOZZZOOOOOOO.OOOOO.OOOOZ$     ') +
+chalk.blue('\n               OOOOOOOOOOOOOOOOOOOOOOOOOOO$O      ') +
+chalk.blue('\n                 OOOOOOOOOO') + chalk.white('..') + chalk.blue('OOOOOOOOZ') + chalk.white('..') + chalk.blue('ZZ        ') +
+chalk.blue('\n                   8OOOOOOOOO') + chalk.white('........') + chalk.blue('O$O          ') +
+chalk.blue('\n                       OOOOOOOOOOOZOD             ') +
+chalk.blue('\n                             ^^                   ') +
+chalk.blue('\n   ___  ___ _   _ ___  _   _      _____ _  _ ___ __  __ ___ ') +
+chalk.blue('\n  |   \\| _ \\ | | | _ \\/_\\ | |    |_   _| || | __|  \\/  | __|') +
+chalk.blue('\n  | |) |   / |_| |  _/ _ \\| |__    | | | __ | _|| |\\/| | _| ') +
+chalk.blue('\n  |___/|_|_\\\\___/|_|/_/ \\_\\____|   |_| |_||_|___|_|  |_|___|') +
+'\n' +
+'\n';
